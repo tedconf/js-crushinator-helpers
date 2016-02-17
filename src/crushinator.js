9def3ae6b9afee0981abf0c6e0d4a37688e9de7d
Joe Bartlett <xo@redoPop.com>
2016-02-17 12:23:13 -0600

    Update source to follow Airbnb style guide

--- src/crushinator.js
+++ src/crushinator.js
@@ -4,6 +4,8 @@ Library of simple JS methods to produce crushed image URLs.
 http://github.com/tedconf/js-crushinator-helpers
 */
 
+'use strict';
+
 /**
 A list of strings and regular expressions
 */
@@ -39,6 +41,35 @@ function extractHost(url) {
 }
 
 /**
+Check to see if a URL passes Crushinator's host whitelist.
+
+@param {string} url
+@returns {boolean}
+*/
+export function crushable(url) {
+  return imageHosts.indexOf(extractHost(url)) !== -1;
+}
+
+/**
+Restore a previously crushed URL to its original form.
+
+@param {string} url
+@returns {string}
+*/
+export function uncrush(url) {
+  const parts = url.match(
+    /(.+)?\/\/(?:img(?:-ssl)?\.tedcdn\.com|tedcdnpi-a\.akamaihd\.net)\/r\/([^?]+)/
+  );
+
+  // Avoid double-crushing images
+  if (parts) {
+    return uncrush(parts[1] + '//' + parts[2]);
+  }
+
+  return url;
+}
+
+/**
 Returns a version of the image URL that uses Crushinator with the
 specified options string:
 
@@ -62,34 +93,6 @@ export function crush(url, options) {
   return 'https://tedcdnpi-a.akamaihd.net/r/' +
     url.replace(/.*\/\//, '') +
     (options ? '?' + options : '');
-}
-
-/**
-Restore a previously crushed URL to its original form.
-
-@param {string} url
-@returns {string}
-*/
-export function uncrush(url) {
-  var parts;
-
-  // Avoid double-crushing images
-  parts = url.match(/(.+)?\/\/(?:img(?:-ssl)?\.tedcdn\.com|tedcdnpi-a\.akamaihd\.net)\/r\/([^?]+)/);
-  if (parts) {
-    return uncrush(parts[1] + '//' + parts[2]);
-  }
-
-  return url;
-}
-
-/**
-Check to see if a URL passes Crushinator's host whitelist.
-
-@param {string} url
-@returns {boolean}
-*/
-export function crushable(url) {
-  return imageHosts.indexOf(extractHost(url)) !== -1;
 }
 
 export default crush;
