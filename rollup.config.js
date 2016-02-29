5009f2d50e4be2dd29b6d9f5a303d4ffa2434de1
Joe Bartlett <xo@redoPop.com>
2016-02-29 16:38:38 -0600

    Import .babelrc in Rollup config
    This is to reduce the risk of error when additional presets and
    transforms are added to our Babel configuration. For the most
    part, our Rollup configuration is identical, with the exception
    of the "es2015" vs. "es2015-rollup" preset.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import babel from 'rollup-plugin-babel';
+import fs from 'fs';
+
+const babelConfig = JSON.parse(fs.readFileSync('.babelrc', 'utf8'));
+
+babelConfig.babelrc = false;
+babelConfig.presets = ['es2015-rollup'];
 
 export default {
   entry: 'src/crushinator.js',
   dest: 'dist/crushinator.js',
   plugins: [
-    babel({
-      babelrc: false,
-      presets: ['es2015-rollup'],
-    }),
+    babel(babelConfig),
   ],
   format: 'umd',
   moduleName: 'crushinator',
