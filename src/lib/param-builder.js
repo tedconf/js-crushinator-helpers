347fa75532174d31cfff97cdf999ab337868b6db
Joe Bartlett <xo@redoPop.com>
2016-03-02 09:04:06 -0600

    Add dehyphenation method to ParamBuilder
    Test added in 14a02e3f23bade09f967005232d5744de1cc1481 now passes.

--- src/lib/param-builder.js
+++ src/lib/param-builder.js
@@ -12,10 +12,36 @@ export class ParamBuilder {
   }
 
   /**
+  Convert values from hyphenated form to an object tree.
+  */
+  dehyphenate(values) {
+    const dehyphenated = {};
+
+    for (const key in values) {
+      if (values.hasOwnProperty(key)) {
+        const value = values[key];
+        const splitted = key.match(/([^-]+)-+(.*)/);
+
+        if (splitted && this.options.hasOwnProperty(splitted[1])) {
+          dehyphenated[splitted[1]] = dehyphenated[splitted[1]] || {};
+          dehyphenated[splitted[1]][splitted[2]] = value;
+        } else {
+          dehyphenated[key] = value;
+        }
+      }
+    }
+
+    return dehyphenated;
+  }
+
+  /**
   Returns parameters in object form.
   */
   get(values) {
     const params = {};
+
+    // Convert "crop-width" to crop.width etc.
+    values = this.dehyphenate(values);
 
     for (const key in this.options) {
       if (values.hasOwnProperty(key)) {
