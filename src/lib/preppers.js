921fbadcb789b993ce1082b7d4abfcb9465006e7
Joe Bartlett <joe@redopop.com>
2017-07-18 17:08:07 -0500

    Provide a fallback option for numeric prepper

--- src/lib/preppers.js
+++ src/lib/preppers.js
@@ -6,17 +6,31 @@ values.
 import { error } from './log';
 
 /**
+Returns true if the value is undefined, null, or false.
+
+@param {*} value - Value to test for emptiness.
+@returns {boolean}
+*/
+export function isBlank(value) {
+  return (
+    typeof value === 'undefined' ||
+    value === null ||
+    value === false
+  );
+}
+
+/**
 Prepare a numerical value.
 
 @param {*} value - Value that should be typecast as a number.
 @returns {number}
 */
-export function prepNumber(value) {
-  let outgoing = Number(value);
+export function prepNumber(value, fallback = 0) {
+  let outgoing = isBlank(value) ? fallback : Number(value);
 
   if (!isFinite(outgoing)) {
     error(`"${value}" is not a finite number`);
-    outgoing = 0;
+    outgoing = fallback;
   }
 
   return outgoing;
