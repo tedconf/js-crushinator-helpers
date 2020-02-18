04d225c1afc573643a76d2bb587cb003e88d69a9
Vince Speelman <vince@ted.com>
2020-02-18 13:56:24 -0500

    chore: prettify output

--- src/index.ts
+++ src/index.ts
@@ -4,47 +4,47 @@ Library of simple JS methods to produce crushed image URLs.
 http://github.com/tedconf/js-crushinator-helpers
 */
 
-import { defaultify } from "./lib/defaultify";
-import { parameterize } from "./lib/parameterize";
-import { serialize } from "./lib/query-string";
-import { desnag } from "./lib/desnag";
-import { CrushConfig } from "./lib/crush-config";
+import { defaultify } from './lib/defaultify';
+import { parameterize } from './lib/parameterize';
+import { serialize } from './lib/query-string';
+import { desnag } from './lib/desnag';
+import { CrushConfig } from './lib/crush-config';
 
 /**
 A whitelist: Crushinator is capable of optimizing images hosted on
 any of these domains.
 */
 export const imageHosts = [
-  "assets.tedcdn.com",
-  "assets2.tedcdn.com",
-  "ems.ted.com",
-  "ems-staging.ted.com",
-  "images.ted.com",
-  "pa.tedcdn.com",
-  "pb-assets.tedcdn.com",
-  "pe.tedcdn.com",
-  "pf.tedcdn.com",
-  "ph.tedcdn.com",
-  "pj.tedcdn.com",
-  "pk.tedcdn.com",
-  "pl.tedcdn.com",
-  "s3.amazonaws.com",
-  "s3-us-west-2.amazonaws.com",
-  "staging.ted.com",
-  "storage.ted.com",
-  "talkstar-photos.s3.amazonaws.com",
-  "tedcdnpa-a.akamaihd.net",
-  "tedcdnpe-a.akamaihd.net",
-  "tedcdnpf-a.akamaihd.net",
-  "tedconfblog.files.wordpress.com",
-  "tedideas.files.wordpress.com",
-  "tedlive.ted.com",
-  "tedlive-staging.ted.com",
-  "ted2017.ted.com",
-  "ted2017-staging.ted.com",
-  "userdata.amara.org",
-  "www.filepicker.io",
-  "www.ted.com"
+  'assets.tedcdn.com',
+  'assets2.tedcdn.com',
+  'ems.ted.com',
+  'ems-staging.ted.com',
+  'images.ted.com',
+  'pa.tedcdn.com',
+  'pb-assets.tedcdn.com',
+  'pe.tedcdn.com',
+  'pf.tedcdn.com',
+  'ph.tedcdn.com',
+  'pj.tedcdn.com',
+  'pk.tedcdn.com',
+  'pl.tedcdn.com',
+  's3.amazonaws.com',
+  's3-us-west-2.amazonaws.com',
+  'staging.ted.com',
+  'storage.ted.com',
+  'talkstar-photos.s3.amazonaws.com',
+  'tedcdnpa-a.akamaihd.net',
+  'tedcdnpe-a.akamaihd.net',
+  'tedcdnpf-a.akamaihd.net',
+  'tedconfblog.files.wordpress.com',
+  'tedideas.files.wordpress.com',
+  'tedlive.ted.com',
+  'tedlive-staging.ted.com',
+  'ted2017.ted.com',
+  'ted2017-staging.ted.com',
+  'userdata.amara.org',
+  'www.filepicker.io',
+  'www.ted.com',
 ];
 
 /**
@@ -53,7 +53,7 @@ level or via the options object by individual helper method calls.
 */
 export const config = {
   defaults: true,
-  host: "https://pi.tedcdn.com"
+  host: 'https://pi.tedcdn.com',
 };
 
 /**
@@ -63,7 +63,7 @@ export const config = {
  * @returns The crushed URL
  */
 function extractHost(url: string): string {
-  return String(url).replace(/.*\/\/([^/]+).*/, "$1");
+  return String(url).replace(/.*\/\/([^/]+).*/, '$1');
 }
 
 /**
@@ -83,7 +83,7 @@ export function crushable(url: string): boolean {
  */
 export function uncrush(url: string): string {
   const parts = String(url).match(
-    /(.+)?\/\/(?:(?:img(?:-ssl)?|pi)\.tedcdn\.com|tedcdnpi-a\.akamaihd\.net)\/r\/([^?]+)/
+    /(.+)?\/\/(?:(?:img(?:-ssl)?|pi)\.tedcdn\.com|tedcdnpi-a\.akamaihd\.net)\/r\/([^?]+)/,
   );
 
   // Avoid double-crushing images
@@ -117,14 +117,14 @@ export function crush(url: string, options: CrushConfig = {}): string {
 
   const params = serialize(
     parameterize(
-      defaultify(Object.assign({ defaults: config.defaults }, options))
-    )
+      defaultify(Object.assign({ defaults: config.defaults }, options)),
+    ),
   );
 
   return desnag(
-    `${config.host}/r/${uncrushed.replace(/.*\/\//, "")}${
-      params ? `?${params}` : ""
-    }`
+    `${config.host}/r/${uncrushed.replace(/.*\/\//, '')}${
+      params ? `?${params}` : ''
+    }`,
   );
 }
 
